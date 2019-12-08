db34767954e75489406d3fe33b624f5da2aa4fe7
Faiz Ikhwan <faizikhwantop@gmail.com>
2019-12-08 22:04:37 +0800

    import module npm

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+// const name = require("./utils.js")
+const validator = require("validator")
+const add = require("./utils.js")
 const fs = require('fs')
+const notes = require("./notes.js")
+const chalk = require("chalk")
 
 fs.writeFileSync('notes.txt', 'My name is Faiz Ikhwan. ')
 fs.appendFileSync("notes.txt", "I am the tech lead. ")
+
+const sum = add(4, 6)
+
+// console.log("name: " + name)
+console.log("sum: " + sum)
+console.log("notes: " + notes())
+console.log(validator.isEmail("faizikhwantop@gmail.com"))
+console.log(chalk.green("Success"))
