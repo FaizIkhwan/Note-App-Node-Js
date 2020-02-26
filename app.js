84bb07fd3bcd1c2998c6b43624fc237d87faf4d4
Faiz Ikhwan <faiz.ikhwan@hexainitiative.com>
2020-02-26 15:53:03 +0800

    [] add handler

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ const yargs = require('yargs')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
-    }
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    },
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
 })
 
 // Create remove command
