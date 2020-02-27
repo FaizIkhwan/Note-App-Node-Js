6beb2b25f78d74a4cc77908f9da693bcda99892a
Faiz Ikhwan <faiz.ikhwan@hexainitiative.com>
2020-02-27 11:29:54 +0800

    [] Removing a note.

--- app.js
+++ app.js
@@ -27,8 +27,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Removing a note',
-    handler: function() {
-        console.log('Removing a note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title)
     }
 })
 
