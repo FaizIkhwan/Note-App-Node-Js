5b28ce81c47a16acd7c85845e43de8779751ae2a
Faiz Ikhwan <faiz.ikhwan@hexainitiative.com>
2020-02-27 10:48:37 +0800

    [] Adding a note.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const getNotes = require("./notes.js")
+const notes = require("./notes.js")
 const chalk = require("chalk")
 const yargs = require('yargs')
 
@@ -7,8 +7,7 @@ yargs.command({
     command: 'add',
     describe: 'Add a new note',
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNotes(argv.title, argv.body)
     },
     builder: {
         title: {
--- notes.js
+++ notes.js
@@ -1,7 +1,43 @@
-console.log("notes.js")
+const fs = require('fs')
 
-const getNotes = function() {
+const getNotes = function () {
     return "Your notes..."
 }
 
-module.exports = getNotes
+const addNotes = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log("New note Added!")
+    } else {
+        console.log("Note title taken!")
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJson = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJson)
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes
+}
