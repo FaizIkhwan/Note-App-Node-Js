b7ecc9e5bd092a8107b0e36385d7df0ebdb27bd4
Faiz Ikhwan <faiz.ikhwan@hexainitiative.com>
2020-02-28 15:21:38 +0800

    [] Reading a note

--- notes.js
+++ notes.js
@@ -7,9 +7,9 @@ const getNotes = () => {
 
 const addNotes = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
     
-    if (duplicateNotes.length === 0) {
+    if (duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -18,6 +18,18 @@ const addNotes = (title, body) => {
         console.log(chalk.green.inverse('New note Added!'))
     } else {
         console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found!'))
     }
 }
 
@@ -57,8 +69,8 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNotes: addNotes,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
